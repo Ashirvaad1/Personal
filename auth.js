6dc9a98191155f634c8feef60fe56305e2347880
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-03-09 14:17:36 +0530

    auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const auth0=new Auth0Client
   ({
-    domain: 'your-auth0-domain',
-    client_id: 'your-client-id'
+    domain: 'dev-rltkinel744hmy2l.us.auth0.com',
+    client_id: 'XP0iyqtFVUtZuvE4hfc0xUKNsfShqgpi'
   });
