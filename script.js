c05991cda2e3e9624dae8bea32268c130a3e3ed6
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-10-23 21:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
   const firebaseConfig = {
     apiKey: "AIzaSyA8aXyZQejDtv_4ErK8o435TCTWsTjebcw",
@@ -10,22 +11,21 @@
     measurementId: "G-WZ96Y9NC7T"
   };
 const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
 document.getElementById('loginButton').addEventListener('click', function() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    auth.signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            window.location.href = 'home.html';
-        })
-        .catch((error) => {
-            document.getElementById('errorMessage').innerText = error.message;
-        });
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.href = 'home.html';
+    })
+    .catch((error) => {
+      document.getElementById('errorMessage').innerText = error.message;
+    });
 });
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        window.location.href = 'home.html';
-    }
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = 'home.html';
+  }
 });
