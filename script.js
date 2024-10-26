ccca546d77b7e11cb93a3634131470e8be8ed777
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-10-26 11:24:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
-  const firebaseConfig = {
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+const firebaseConfig = {
     apiKey: "AIzaSyA8aXyZQejDtv_4ErK8o435TCTWsTjebcw",
     authDomain: "infinity-run-1.firebaseapp.com",
     projectId: "infinity-run-1",
@@ -9,29 +9,30 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https:/
     messagingSenderId: "424611937147",
     appId: "1:424611937147:web:e49291921391b5842c1bde",
     measurementId: "G-WZ96Y9NC7T"
-  };
+};
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-document.getElementById('loginButton').addEventListener('click', function() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
- signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    console.log("Login successful:", userCredential);
-    window.location.href = 'home.html';  // Redirect to home.html
-  })
-  .catch((error) => {
-    console.log("Login error:", error.message);
-    document.getElementById('errorMessage').innerText = error.message;
-  });
 onAuthStateChanged(auth, (user) => {
   const loadingElement = document.getElementById('loading');
   const mainContent = document.getElementById('mainContent');
-  if (!user) {
-    window.location.href = 'index.html';
+  if (user) {
+    window.location.href = 'home.html';
   } else {
     loadingElement.style.display = 'none';
     mainContent.style.display = 'block';
   }
+});
+document.getElementById('loginButton').addEventListener('click', function () {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log("Login successful:", userCredential);
+      window.location.href = 'home.html';
+    })
+    .catch((error) => {
+      console.log("Login error:", error.message);
+      document.getElementById('errorMessage').innerText = error.message;
+    });
 });
